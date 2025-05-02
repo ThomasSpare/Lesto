64491494059ec97f019ddbdba5d3e57905b1a249
ThomasSpare <t.spare.jkpg@gmail.com>
2025-05-02 17:56:16 +0200

    flash menu in hamburger test

--- frontend/src/pages/WhatIs.js
+++ frontend/src/pages/WhatIs.js
@@ -16,10 +16,9 @@ const WhatIs = () => {
       <main className="content">
         <section className="text-section">
           <h2>About LESTO</h2>
-          <blockquote className="quote"></blockquote>
-          <ul className="WhatIs_list">
-            <li></li>
-          </ul>
+          <p className="WhatIs_list">
+          Lead Fast Reactors (LFRs) are a crucial technology for advancing both hydrogen energy and nuclear power solutions, offering a promising and ambitious approach. Investors are already drawn to the advantages of competitive small- to medium-sized LFRs and their potential impact on the global market. The EU-funded LESTO project aims to further develop this technology, advancing it along its established roadmap and demonstrating its safety, efficiency, and other key features. To achieve this, the project will leverage the most relevant facilities in Europe and the UK to thoroughly validate, assess, and assist in the continued development of LFR technology.
+        </p>
         </section>
       </main>
     </div>
